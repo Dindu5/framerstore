4e97209c0f90435ffef6171aec01af0fc58a8229
Dindu5 <chimdindue@gmail.com>
2023-12-17 22:08:20 +0100

    fix: updated template component to add hover effect

--- components/TemplateCard/index.vue
+++ components/TemplateCard/index.vue
@@ -1,6 +1,22 @@
 <template>
-  <div class="template-card">
-    <img :src="cardData.img" alt="Template Image" />
+  <div
+    class="template-card"
+    @mouseover="showCta = true"
+    @mouseout="showCta = false"
+  >
+    <div class="template-card__img">
+      <img :src="cardData.img" alt="Template Image" />
+      <div :class="showCta ? 'template-card__img-cta' : ''">
+        <button class="btn btn-icon">
+          <span class="material-symbols-rounded"> bookmark </span>
+        </button>
+        <button class="btn btn-icon">
+          <span class="material-symbols-rounded"> upload </span>
+        </button>
+        <button class="btn">Live preview</button>
+      </div>
+    </div>
+
     <div class="template-card__details">
       <h4>{{ cardData.title }}</h4>
       <p>{{ cardData.price }}</p>
@@ -20,6 +36,12 @@ export default {
       default: () => {},
     },
   },
+  data() {
+    return {
+      showCta: false,
+    };
+  },
+  methods: {},
 };
 </script>
 
