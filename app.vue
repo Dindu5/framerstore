e96d7cd5b9d9a8aaaf7a54ac40d0a19874d9e19d
Dindu5 <chimdindue@gmail.com>
2024-01-15 22:23:39 +0100

    removed meta

--- app.vue
+++ app.vue
@@ -5,56 +5,56 @@
 </template>
 
 <script setup lang="ts">
-useHead({
-  title: "Framerstore",
-  meta: [
-    {
-      name: "description",
-      content: "Discover the best suited Framer template for your website",
-    },
-    {
-      name: "application-name",
-      content: "FramerStore",
-    },
-    {
-      name: "keywords",
-      content:
-        "Template, Templates, Framer, framerstore, website, web templates",
-    },
-    {
-      name: "robots",
-      content: "index, follow",
-    },
-    {
-      name: "og:title",
-      content: "The Rock",
-    },
-    {
-      name: "og:image",
-      content:
-        "https://res.cloudinary.com/dindu/image/upload/v1705347384/framerstore-og_jdeqlq_ypj8ru.png",
-    },
-    {
-      name: "twitter:card",
-      content: "summary_large_image",
-    },
-    {
-      name: "og:description",
-      content: "Discover the best suited Framer template for your website",
-    },
-    {
-      name: "twitter:description",
-      content: "Discover the best suited Framer template for your website",
-    },
-    {
-      name: "twitter:image",
-      content:
-        "https://res.cloudinary.com/dindu/image/upload/v1705347384/framerstore-og_jdeqlq_ypj8ru.png",
-    },
-    {
-      name: "og:type",
-      content: "website",
-    },
-  ],
-});
+// useHead({
+//   title: "Framerstore",
+//   meta: [
+//     {
+//       name: "description",
+//       content: "Discover the best suited Framer template for your website",
+//     },
+//     {
+//       name: "application-name",
+//       content: "FramerStore",
+//     },
+//     {
+//       name: "keywords",
+//       content:
+//         "Template, Templates, Framer, framerstore, website, web templates",
+//     },
+//     {
+//       name: "robots",
+//       content: "index, follow",
+//     },
+//     {
+//       name: "og:title",
+//       content: "The Rock",
+//     },
+//     {
+//       name: "og:image",
+//       content:
+//         "https://res.cloudinary.com/dindu/image/upload/v1705347384/framerstore-og_jdeqlq_ypj8ru.png",
+//     },
+//     {
+//       name: "twitter:card",
+//       content: "summary_large_image",
+//     },
+//     {
+//       name: "og:description",
+//       content: "Discover the best suited Framer template for your website",
+//     },
+//     {
+//       name: "twitter:description",
+//       content: "Discover the best suited Framer template for your website",
+//     },
+//     {
+//       name: "twitter:image",
+//       content:
+//         "https://res.cloudinary.com/dindu/image/upload/v1705347384/framerstore-og_jdeqlq_ypj8ru.png",
+//     },
+//     {
+//       name: "og:type",
+//       content: "website",
+//     },
+//   ],
+// });
 </script>
