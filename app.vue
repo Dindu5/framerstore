bbeb1bc37003569137f79ce5d1c8c0be8015f12f
Dindu5 <chimdindue@gmail.com>
2024-01-15 22:02:02 +0100

    updated meta tags

--- app.vue
+++ app.vue
@@ -25,6 +25,36 @@ useHead({
       name: "robots",
       content: "index, follow",
     },
+    {
+      name: "og:title",
+      content: "The Rock",
+    },
+    {
+      name: "og:image",
+      content:
+        "https://res.cloudinary.com/dindu/image/upload/v1705347384/framerstore-og_jdeqlq_ypj8ru.png",
+    },
+    {
+      name: "twitter:card",
+      content: "summary_large_image",
+    },
+    {
+      name: "og:description",
+      content: "Discover the best suited Framer template for your website",
+    },
+    {
+      name: "twitter:description",
+      content: "Discover the best suited Framer template for your website",
+    },
+    {
+      name: "twitter:image",
+      content:
+        "https://res.cloudinary.com/dindu/image/upload/v1705347384/framerstore-og_jdeqlq_ypj8ru.png",
+    },
+    {
+      name: "og:type",
+      content: "website",
+    },
   ],
 });
 </script>
